303c4e24dc77adb64e87e92056dbff129af26ec1
Pulichev <vlad.pulichev777@gmail.com>
2017-02-06 20:26:56 +0300

    Deleted old files

--- SpotMap/CustomPinAnnotation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustomPinAnnotation.swift
-//  SpotMap
-//
-//  Created by Владислав Пуличев on 21.01.17.
-//  Copyright © 2017 Владислав Пуличев. All rights reserved.
-//
-
-import Foundation

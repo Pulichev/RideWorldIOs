02e1a202a24c68013239af5c3c44af79f6177a00
Pulichev <vlad.pulichev777@gmail.com>
2017-02-20 21:27:56 +0300

    Make only 2 tabs now. lib - camera (ph/video) Created architecture

---
+++ SpotMap/Controllers/CameraControllers/TakeMediaFromCameraController.swift
@@ -0,0 +1,10 @@
+//
+//  TakeMediaFromCameraController.swift
+//  SpotMap
+//
+//  Created by Владислав Пуличев on 20.02.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import Foundation
+
---
+++ SpotMap/Controllers/CameraControllers/TakeMediaFromLibrary.swift
@@ -0,0 +1,10 @@
+//
+//  TakeMediaFromLibrary.swift
+//  SpotMap
+//
+//  Created by Владислав Пуличев on 20.02.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import Foundation
+

e805440f12e806b7bd7db835e2f9d9fca7ec9187
Pulichev <vlad.pulichev777@gmail.com>
2017-02-26 22:46:14 +0300

    some adds to edit profile

--- SpotMap/Controllers/EditProfileController.swift
+++ SpotMap/Controllers/EditProfileController.swift
@@ -9,9 +9,20 @@
 import Foundation
 import UIKit
 
-class EditProfileController: UIViewController {
+class EditProfileController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var backendless: Backendless!
+    
     @IBOutlet var tableView: UITableView!
     @IBOutlet var userPhoto: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        self.backendless = Backendless.sharedInstance()
+    }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         
@@ -23,26 +34,76 @@ class EditProfileController: UIViewController {
     
     //Main table filling region
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 2
-        } else {
-            if section == 1 {
-                return 2
-            }
-        }
+        return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SpotPostsCell", for: indexPath) as! SpotPostsCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EditProfileCell", for: indexPath) as! EditProfileCell
         let row = indexPath.row
         
+        let leftImageView = UIImageView()
+        let leftView = UIView()
+        leftView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        leftImageView.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
         
+        cell.field.leftViewMode = .always
+        
+        switch row {
+        case 0:
+            cell.field.placeholder = "Enter new name and sename"
+            leftImageView.image = UIImage(named: "nameAndSename.png")
+            leftView.addSubview(leftImageView)
+            cell.field.leftView = leftView
+            break
+        case 1:
+            cell.field.placeholder = "Enter new bio description"
+            leftImageView.image = UIImage(named: "biography.png")
+            leftView.addSubview(leftImageView)
+            cell.field.leftView = leftView
+            break
+        case 2:
+            cell.field.placeholder = "Enter new login"
+            leftImageView.image = UIImage(named: "login.png")
+            leftView.addSubview(leftImageView)
+            cell.field.leftView = leftView
+            break
+
+        case 3:
+            cell.field.placeholder = "Enter new email"
+            leftImageView.image = UIImage(named: "email.ico")
+            leftView.addSubview(leftImageView)
+            cell.field.leftView = leftView
+            break
+
+        default:
+            break
+        }
         
         return cell
     }
+    
+    var keyBoardAlreadyShowed = false //using this to not let app to scroll view
+    //if we tapped UITextField and then another UITextField
+}
 
+extension EditProfileController {
+    func keyboardWillShow(notification: NSNotification) {
+        if !keyBoardAlreadyShowed {
+            self.view.frame.origin.y -= 100
+            keyBoardAlreadyShowed = true
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y += 100
+        keyBoardAlreadyShowed = false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }

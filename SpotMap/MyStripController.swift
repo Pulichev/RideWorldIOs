dbb6a3b1c1a9b2711503abb42caa854d45a15431
Pulichev <vlad.pulichev777@gmail.com>
2017-03-01 19:46:27 +0300

    trying to implement google drive API Its like temp code in wrong controller

--- SpotMap/MyStripController.swift
+++ SpotMap/MyStripController.swift
@@ -7,13 +7,134 @@
 //
 
 import Foundation
+import GoogleAPIClientForREST
+import GTMOAuth2
+import UIKit
 
 class MyStripController: UIViewController {
+    private let kKeychainItemName = "Drive API"
+    private let kClientID = "AIzaSyAEWGOwN4hvvRp8WVUCOLppJl8zXRRuvOY"
     
-    var backendless: Backendless!
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLRAuthScopeDriveReadonly]
     
+    private let service = GTLRDriveService()
+    let output = UITextView()
+    
+    // When the view loads, create necessary subviews
+    // and initialize the Drive API service
     override func viewDidLoad() {
-        //some tests
-        self.backendless = Backendless.sharedInstance()
+        super.viewDidLoad()
+        
+        output.frame = view.bounds
+        output.isEditable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        view.addSubview(output);
+        
+        if let auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychain(
+            forName: kKeychainItemName,
+            clientID: kClientID,
+            clientSecret: nil) {
+            service.authorizer = auth
+        }
+        
+    }
+    
+    // When the view appears, ensure that the Drive API service is authorized
+    // and perform API calls
+    override func viewDidAppear(_ animated: Bool) {
+        if let authorizer = service.authorizer,
+            let canAuth = authorizer.canAuthorize, canAuth {
+            listFiles()
+        } else {
+            present(
+                createAuthController(),
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
+    // List up to 10 files in Drive
+    func listFiles() {
+        let query = GTLRDriveQuery_FilesList.query()
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: Selector(("displayResultWithTicket:finishedWithObject:error:"))
+        )
+    }
+    
+    // Process the response and display output
+    func displayResultWithTicket(ticket: GTLRServiceTicket,
+                                 finishedWithObject result : GTLRDrive_FileList,
+                                 error : NSError?) {
+        
+        if let error = error {
+            showAlert(title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+        var text = "";
+        if (result.files!.count != 0) {
+            text += "Files:\n"
+            for file in result.files! {
+                text += "\(file.name) (\(file.identifier))\n"
+            }
+        } else {
+            text += "No files found."
+        }
+        output.text = text
+    }
+    
+    // Creates the auth controller for authorizing access to Drive API
+    private func createAuthController() -> GTMOAuth2ViewControllerTouch {
+        let scopeString = scopes.joined(separator: " ")
+        return GTMOAuth2ViewControllerTouch(
+            scope: scopeString,
+            clientID: kClientID,
+            clientSecret: nil,
+            keychainItemName: kKeychainItemName,
+            delegate: self,
+            finishedSelector: Selector(("viewController:finishedWithAuth:error:"))
+        )
+    }
+    
+    // Handle completion of the authorization process, and update the Drive API
+    // with the new credentials.
+    func viewController(vc : UIViewController,
+                        finishedWithAuth authResult : GTMOAuth2Authentication, error : NSError?) {
+        
+        if let error = error {
+            service.authorizer = nil
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            return
+        }
+        
+        service.authorizer = authResult
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }

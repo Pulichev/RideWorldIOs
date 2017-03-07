f9df269e6c1c3b8d0240c112ef7a00fe7a7f58e9
Pulichev <vlad.pulichev777@gmail.com>
2017-03-07 18:07:23 +0300

    tests

---
+++ SpotMap/Data/LikeItem.swift
@@ -0,0 +1,13 @@
+//
+//  LikeItem.swift
+//  SpotMap
+//
+//  Created by Владислав Пуличев on 07.03.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import Foundation
+
+class LikeItem {
+    
+}

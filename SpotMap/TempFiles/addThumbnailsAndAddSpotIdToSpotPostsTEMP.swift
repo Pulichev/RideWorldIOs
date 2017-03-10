5d7e50251acfb173a281e8b8116ac80f90188d65
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-03-11 00:12:11 +0300

    successfully replaced spotPost files, created thumbnails for images.

--- SpotMap/TempFiles/addThumbnailsAndAddSpotIdToSpotPostsTEMP.swift
+++ SpotMap/TempFiles/addThumbnailsAndAddSpotIdToSpotPostsTEMP.swift
@@ -27,43 +27,84 @@ class addThumbnailsAndAddSpotIdToSpotPostsTEMP  {
                             spotPostDBRef.observe(.value, with: { snapshot2 in
                                 let spotPostItem = SpotPostItem(snapshot: snapshot2)
                                 // adding to post spotId
-                                spotPostDBRef.setValue([
-                                    "addedByUser" : spotPostItem.addedByUser,
-                                    "createdDate" : spotPostItem.createdDate,
-                                    "description" : spotPostItem.description,
-                                    "isPhoto" : spotPostItem.isPhoto,
-                                    "key" : spotPostItem.key,
-                                    "spotId" : spotDetailsItem.key
-                                    ])
+//                                spotPostDBRef.setValue([
+//                                    "addedByUser" : spotPostItem.addedByUser,
+//                                    "createdDate" : spotPostItem.createdDate,
+//                                    "description" : spotPostItem.description,
+//                                    "isPhoto" : spotPostItem.isPhoto,
+//                                    "key" : spotPostItem.key,
+//                                    "spotId" : spotDetailsItem.key
+//                                    ])
                                 
                                 // transfering photos to new location
                                 if spotPostItem.isPhoto {
-                                    let storageRef = FIRStorage.storage().reference(withPath: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + postLink.key + ".jpeg")
-                                    
-                                    
-                                    
-                                    // create thumbnail
+                                    let storageRef = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + postLink.key + ".jpeg")
+                                    storageRef.data(withMaxSize: 10 * 1024 * 1024) { data, error in
+                                        if let error = error {
+                                            // Uh-oh, an error occurred!
+                                            print(error.localizedDescription)
+                                        } else {
+                                            let newStorageRef = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + spotDetailsItem.key + "/" + postLink.key + ".jpeg")
+                                            newStorageRef.put(data!, metadata: nil) { (metadata, error) in
+                                                guard let metadata = metadata else {
+                                                    // Uh-oh, an error occurred!
+                                                    return
+                                                }
+                                                // Metadata contains file metadata such as size, content-type, and download URL.
+                                                let downloadURL = metadata.downloadURL
+                                            }
+                                            
+                                            // create thumbnail
+                                            let image = UIImage(data: data!)
+                                            let resizedImage = ResizeImage.resize(image: image!, targetSize: CGSize(width: 150.0, height: 150.0))
+                                            let compressedImageData: Data = UIImageJPEGRepresentation(resizedImage, 0.8)!
+                                            let newStorageRefForThumbnail = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + spotDetailsItem.key + "/" + postLink.key + "_thumbnail.jpeg")
+                                            newStorageRefForThumbnail.put(compressedImageData, metadata: nil) { (metadata, error) in
+                                                guard let metadata = metadata else {
+                                                    // Uh-oh, an error occurred!
+                                                    return
+                                                }
+                                                // Metadata contains file metadata such as size, content-type, and download URL.
+                                                let downloadURL = metadata.downloadURL
+                                            }
+                                        }
+                                    }
                                 } else {
-                                    let storageRef = FIRStorage.storage().reference(withPath: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + postLink.key + ".m4v")
-                                    let storageRefForThumbnail = FIRStorage.storage().reference(withPath: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + postLink.key + "_thumbnail.jpeg")
+                                    let storageRef = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + postLink.key + ".m4v")
+                                    let storageRefForThumbnail = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + postLink.key + "_thumbnail.jpeg")
                                     
                                     //transfer
                                     storageRef.data(withMaxSize: 10 * 1024 * 1024) { data, error in
                                         if let error = error {
                                             // Uh-oh, an error occurred!
-                                            print("ERROR IN DOWNLOAD PART!!!")
+                                            print(error.localizedDescription)
                                         } else {
-                                            let newStorageRef = FIRStorage.storage().reference(withPath: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + spotDetailsItem.key + "/" + postLink.key + ".jpeg")
-                                            let image = UIImage(data: data!)
+                                            let newStorageRef = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + spotDetailsItem.key + "/" + postLink.key + ".m4v")
+                                            newStorageRef.put(data!, metadata: nil) { (metadata, error) in
+                                                guard let metadata = metadata else {
+                                                    // Uh-oh, an error occurred!
+                                                    return
+                                                }
+                                                // Metadata contains file metadata such as size, content-type, and download URL.
+                                                let downloadURL = metadata.downloadURL
+                                            }
+
                                         }
                                     }
                                     storageRefForThumbnail.data(withMaxSize: 10 * 1024 * 1024) { data, error in
                                         if let error = error {
                                             // Uh-oh, an error occurred!
-                                            print("ERROR IN DOWNLOAD PART!!!")
+                                            print(error.localizedDescription)
                                         } else {
-                                            let newStorageRef = FIRStorage.storage().reference(withPath: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + spotDetailsItem.key + "/" + postLink.key + ".jpeg")
-                                            let image = UIImage(data: data!)
+                                            let newStorageRef = FIRStorage.storage().reference(forURL: "gs://spotmap-e3116.appspot.com/media/spotPostMedia/" + spotDetailsItem.key + "/" + postLink.key + "_thumbnail.jpeg")
+                                            newStorageRef.put(data!, metadata: nil) { (metadata, error) in
+                                                guard let metadata = metadata else {
+                                                    // Uh-oh, an error occurred!
+                                                    return
+                                                }
+                                                // Metadata contains file metadata such as size, content-type, and download URL.
+                                                let downloadURL = metadata.downloadURL
+                                            }
                                         }
                                     }
                                 }

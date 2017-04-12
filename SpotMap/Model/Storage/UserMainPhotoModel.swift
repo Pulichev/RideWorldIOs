5124902f65b1749c48151eac460ce3bdee535736
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-04-12 18:58:11 +0300

    fixed link generating

--- SpotMap/Model/Storage/UserMainPhotoModel.swift
+++ SpotMap/Model/Storage/UserMainPhotoModel.swift
@@ -11,7 +11,8 @@ import FirebaseStorage
 class UserMainPhotoModel {
     static func uploadUserMainPhoto(userId: String, image: UIImage, sizePx: Double) {
         let resizedPhoto = ImageManipulations.resize(image: image, targetSize: CGSize(width: sizePx, height: sizePx))
-        let sizePxString = String(describing: sizePx)
+        let sizePxInt = Int(sizePx) // to generate link properly. It doesn't have ".0" in sizes
+        let sizePxString = String(describing: sizePxInt)
         let userPhotoRef = FIRStorage.storage().reference(withPath: "media/userMainPhotoURLs").child(userId + "_resolution" + sizePxString + "x" + sizePxString + ".jpeg")
         //with low compression
         let dataLowCompressionFor: Data = UIImageJPEGRepresentation(resizedPhoto, 0.8)!

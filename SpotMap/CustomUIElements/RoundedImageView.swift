e21c7af53eb6c2049bd6d9ba5f3532861f127499
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-04-19 19:49:18 +0300

    Finished

--- SpotMap/CustomUIElements/RoundedImageView.swift
+++ SpotMap/CustomUIElements/RoundedImageView.swift
@@ -12,20 +12,18 @@ import UIKit
 class RoundedImageView: UIImageView {
     override init(image: UIImage?) {
         super.init(image: image)
-        super.layer.cornerRadius = super.frame.size.height / 2
-        self.layer.cornerRadius = self.frame.size.height / 2
-        
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        super.layer.cornerRadius = super.frame.size.height / 2
-        self.layer.cornerRadius = self.frame.size.height / 2
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        super.layer.cornerRadius = super.frame.size.height / 2
+    }
+    
+    override func layoutSubviews() {
         self.layer.cornerRadius = self.frame.size.height / 2
+        self.clipsToBounds = true
     }
 }

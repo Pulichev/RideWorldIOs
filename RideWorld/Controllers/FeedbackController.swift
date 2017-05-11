969b21a5a7e83f89f2640cb071ed1ac932472fee
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-05-11 20:36:29 +0300

    continuing implementing cells for feedback

---
+++ RideWorld/Controllers/FeedbackController.swift
@@ -0,0 +1,15 @@
+//
+//  FeedbackController.swift
+//  RideWorld
+//
+//  Created by Владислав Пуличев on 11.05.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackController: UIViewController {
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+}
---
+++ RideWorld/CustomUIElements/Cells for feedback/FollowerFBCell.swift
@@ -0,0 +1,24 @@
+//
+//  FollowerFBCell.swift
+//  RideWorld
+//
+//  Created by Владислав Пуличев on 11.05.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import UIKit
+
+class FollowerFBCell: UITableViewCell { // FB = feedback
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

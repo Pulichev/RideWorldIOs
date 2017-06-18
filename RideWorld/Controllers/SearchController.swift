a0ebafe3f32a1fa395f3591f9881423eb52e4625
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-06-18 21:52:49 +0300

    continuing implementing search

--- RideWorld/Controllers/SearchController.swift
+++ RideWorld/Controllers/SearchController.swift
@@ -13,9 +13,7 @@ class SearchController: UITableViewController {
    
    // MARK: - Properties
    var riders = [UserItem]()
-   var filteredRiders = [UserItem]()
    var spots = [SpotItem]()
-   var filteredSpots = [SpotItem]()
    
    let searchController = UISearchController(searchResultsController: nil)
    var selectedScope = "Riders" // default value is "Riders"
@@ -29,6 +27,7 @@ class SearchController: UITableViewController {
       searchController.searchBar.delegate = self
       definesPresentationContext = true
       searchController.dimsBackgroundDuringPresentation = false
+      searchController.searchBar.autocapitalizationType = .words
       
       // Setup the Scope Bar
       searchController.searchBar.scopeButtonTitles = ["Riders", "Spots"]
@@ -74,7 +73,7 @@ class SearchController: UITableViewController {
          
          if spot.mainPhotoRef != nil {
             let spotPhotoURL = URL(string: spot.mainPhotoRef)
-         
+            
             cell.photo.kf.setImage(with: spotPhotoURL)
          }
          cell.name!.setTitle(spot.name, for: .normal)
@@ -88,17 +87,29 @@ class SearchController: UITableViewController {
    func filterContentForSearchText(_ searchText: String) {
       switch self.selectedScope {
       case "Riders":
-         UserModel.searchUsersWithLogin(startedWith: searchText) { users in
-            self.riders = users
+         if searchText.characters.count == 1 {
+            // get items from db where 1st symbol is entered character
+            UserModel.searchUsersWithLogin(startedWith: searchText) { users in
+               self.riders = users
+               
+               self.tableView.reloadData()
+            }
+         } else {
+            // filter items from already downloaded from db
             
-            self.tableView.reloadData()
          }
          
       case "Spots":
-         Spot.searchSpotsWithName(startedWith: searchText) { spots in
-            self.spots = spots
+         if searchText.characters.count == 1 {
+            // get items from db where 1st symbol is entered character
+            Spot.searchSpotsWithName(startedWith: searchText) { spots in
+               self.spots = spots
+               
+               self.tableView.reloadData()
+            }
+         } else {
+            // filter items from already downloaded from db
             
-            self.tableView.reloadData()
          }
          
       default: break

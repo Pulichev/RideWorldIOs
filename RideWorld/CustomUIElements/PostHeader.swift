dd1615f98cb820645dd7db37768c9ce16830345a
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-07-03 23:51:06 +0300

    post header started

---
+++ RideWorld/CustomUIElements/PostHeader.swift
@@ -0,0 +1,24 @@
+//
+//  PostHeader.swift
+//  RideWorld
+//
+//  Created by Владислав Пуличев on 03.07.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import UIKit
+
+class PostHeader: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

50978c8f2362b8b2291414b143003f0ff0719da0
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-07-15 16:25:41 +0300

    added server side script to add posts to feed after follow

--- functions/index.js
+++ functions/index.js
@@ -20,56 +20,53 @@ exports.updateFeed = functions.database.ref('/MainDataBase/usersposts/{userId}/{
          followersRef.once("value", function(snap) {
                            snap.forEach(function(childSnapshot) {
                                         let followerId = childSnapshot.key;
-                                        admin.database().ref('/userpostsfeed/' + followerId + '/' + postId).remove();
+                                        admin.database().ref('/MainDataBase/userpostsfeed/' + followerId + '/' + postId).remove();
                                         console.log('Removed post from feed of user: '+ followerId);
                                         });
                            });
          
-         admin.database().ref('/userpostsfeed/' + userId + '/' + postId).remove(); // remove post from users post strip
+         admin.database().ref('/MainDataBase/userpostsfeed/' + userId + '/' + postId).remove(); // remove post from users post strip
          } else {
          // post was added
          followersRef.once("value", function(snap) {
                            snap.forEach(function(childSnapshot) {
                                         let followerId = childSnapshot.key;
-                                        admin.database().ref('/userpostsfeed/' + followerId + '/' + postId).set(event.data.val());
+                                        admin.database().ref('/MainDataBase/userpostsfeed/' + followerId + '/' + postId).set(event.data.val());
                                         console.log('Added post to feed of user: '+ followerId);
                                         });
                            });
          
-         admin.database().ref('/userpostsfeed/' + userId + '/' + postId).set(event.data.val()); // add post to users post strip
+         admin.database().ref('/MainDataBase/userpostsfeed/' + userId + '/' + postId).set(event.data.val()); // add post to users post strip
          }
          });
 
-// add posts to feed on follow starting // TODO:
-exports.addPostsToNewFollowerFeed = functions.database.ref('/MainDataBase/usersposts/{userId}/{postId}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add posts to feed on follow starting
+exports.addPostsToNewFollowerFeed = functions.database.ref('/MainDataBase/usersfollowers/{userId}/{followerId}')
 .onWrite(event => {
          const userId = event.params.userId;
-         const postId = event.params.postId;
+         const followerId = event.params.followerId;
          
-         let followersRef = admin.database().ref('/MainDataBase/usersfollowers/' + userId);
-         if(!event.data.val()){
-         // post was deleted
-         followersRef.once("value", function(snap) {
-                           snap.forEach(function(childSnapshot) {
-                                        let followerId = childSnapshot.key;
-                                        admin.database().ref('/userpostsfeed/' + followerId + '/' + postId).remove();
-                                        console.log('Removed post from feed of user: '+ followerId);
-                                        });
-                           });
-         
-         admin.database().ref('/userpostsfeed/' + userId + '/' + postId).remove(); // remove post from users post strip
-         } else {
-         // post was added
-         followersRef.once("value", function(snap) {
-                           snap.forEach(function(childSnapshot) {
-                                        let followerId = childSnapshot.key;
-                                        admin.database().ref('/userpostsfeed/' + followerId + '/' + postId).set(event.data.val());
-                                        console.log('Added post to feed of user: '+ followerId);
-                                        });
-                           });
-         
-         admin.database().ref('/userpostsfeed/' + userId + '/' + postId).set(event.data.val()); // add post to users post strip
-         }
+         let refToUserPosts = admin.database().ref('/MainDataBase/usersposts/' + userId);
+         refToUserPosts.once("value", function(snap) {
+                             snap.forEach(function(childSnapshot) {
+                                          let postId = childSnapshot.key;
+                                          admin.database().ref('/MainDataBase/userpostsfeed/' + followerId + '/' + postId).set(childSnapshot.val());
+                                          console.log('Added post to feed of user: '+ followerId);
+                                          });
+                             });
          });
 
 // remove posts from feed on follow ending

b525c1b73ed1a1a40cd502fa3748b50328f695c2
Владислав Пуличев <vlad.pulichev777@gmail.com>
2017-08-11 20:04:11 +0300

    started implementing reset password

---
+++ RideWorld/Controllers/PasswordResetController.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordResetController.swift
+//  RideWorld
+//
+//  Created by Владислав Пуличев on 11.08.17.
+//  Copyright © 2017 Владислав Пуличев. All rights reserved.
+//
+
+import UIKit
+
+class PasswordResetController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
